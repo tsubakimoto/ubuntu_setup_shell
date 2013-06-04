fbd8af38b221b09d044c437c7a31682892a831ac
tsubakimoto_s <dev.tsubaki@gmail.com>
2013-06-04 21:38:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 #update and upgrade
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 #グローバルメニューを無効にする
-sudo apt-get remove --purge firefox-globalmenu thunderbird-globalmenu appmenu-gtk appmenu-gtk3 appmenu-qt appmenu-qt5 indicator-appmenu
+sudo apt-get -y remove --purge firefox-globalmenu thunderbird-globalmenu appmenu-gtk appmenu-gtk3 appmenu-qt appmenu-qt5 indicator-appmenu
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides '@a{sv} {"Gtk/ShellShowsAppMenu": <int32 0>}'
 
 #オーバーレイスクロールバーを無効にする
-sudo apt-get remove overlay-scrollbar
+sudo apt-get -y remove overlay-scrollbar
 
 #Ubuntu Web Appsを削除する
-sudo apt-get remove unity-webapps-common xul-ext-websites-integration
+sudo apt-get -y remove unity-webapps-common xul-ext-websites-integration
 
 #Ubuntu Oneを削除する
-sudo apt-get remove ubuntuone-client python-ubuntuone-client python-ubuntuone-storageprotocol
+sudo apt-get -y remove ubuntuone-client python-ubuntuone-client python-ubuntuone-storageprotocol
 
 #タイトルバーのボタンを左から右側に移動させる
 dconf write /org/gnome/desktop/wm/preferences/button-layout '":minimize,maximize,close"'
@@ -24,20 +24,20 @@ dconf write /org/gnome/desktop/wm/preferences/button-layout '":minimize,maximize
 env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
 
 #IME
-sudo apt-get install ibus-mozc
+sudo apt-get -y install ibus-mozc
 
 #chromium
-sudo apt-get update
-sudo apt-get install chromium-browser
+sudo apt-get -y update
+sudo apt-get -y install chromium-browser
 
 #ubuntu-tweak
 sudo add-apt-repository ppa:tualatrix/ppa
-sudo apt-get update
-sudo apt-get install ubuntu-tweak
+sudo apt-get -y update
+sudo apt-get -y install ubuntu-tweak
 
 #git
-sudo apt-get update
-sudo apt-get install git-core
+sudo apt-get -y update
+sudo apt-get -y install git-core
 
 #Apache
 cd /usr/local/src
@@ -63,7 +63,7 @@ sudo make
 sudo make install
 
 #C++ Compiler
-sudo apt-get install build-essential
+sudo apt-get -y install build-essential
 
 #Apache
 cd /usr/local/src/httpd-2.4.4
@@ -72,4 +72,4 @@ sudo make
 sudo make install
 
 #最後にupdate
-sudo apt-get update
+sudo apt-get -y update
