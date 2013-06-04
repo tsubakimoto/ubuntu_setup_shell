d181a1aa569bf4abdada59f152eb9de7f28c6006
tsubakimoto_s <dev.tsubaki@gmail.com>
2013-06-04 19:19:57 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,8 +54,23 @@ sudo mv apr-1.4.6 apr
 sudo mv apr-util-1.4.1 apr-util
 
 #PCRE
+cd /usr/local/src
 sudo wget http://downloads.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.gz
 sudo tar zxvf pcre-8.33.tar.gz
+cd pcre-8.33
+sudo ./configure
+sudo make
+sudo make install
+
+#C++ Compiler
+sudo apt-get install build-essential
+
+#Apache
+cd /usr/local/src/httpd-2.4.4
+sudo ./configure
+sudo make
+sudo make install
+
 
 #最後にupdate
 sudo apt-get update
